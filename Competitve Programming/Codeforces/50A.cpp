e22870e2876ffb25ce1436dc7ce28878cf57f96a
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-09-19 12:52:56 +0530

    first commit

---
+++ Competitve Programming/Codeforces/50A.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    
+    cout<<(m*n)/2;
+    return 0;
+
+}
