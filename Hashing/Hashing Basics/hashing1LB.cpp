e62b4291c9c11f0d9267673a883c1ac0e5dd2df3
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-01-01 11:28:39 +0530

    Hashing

---
+++ Hashing/Hashing Basics/hashing1LB.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+     string arr[] = {"ab" , "abc","abcc"};
+     cout<< str.size();
+
+    // map<char,int> mp;
+    // int n = str.length();
+
+    // for(auto a : words )
+    // {
+    //     for(char x : a)
+    //     {
+    //         mp[x]++;
+    //     }
+    // }
+
+    // for (auto i : mp)
+    // {
+    //     cout<<i.first << " " << i.second <<endl;
+    // }
+    // return 0;
+}
+
+
+in}
